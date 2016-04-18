7d396ac6522eec7bae3d14d696262f893ca0b32c
Pranav Dhar <pranav.dhar2@gmail.com>
2016-04-18 17:31:36 -0400

    initial commit

---
+++ PropertyTycoon/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PropertyTycoon.MvcApplication" Language="C#" %>
---
+++ PropertyTycoon/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PropertyTycoon.Startup))]
+namespace PropertyTycoon
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
