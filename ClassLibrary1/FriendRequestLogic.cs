b09ec9f409afccad4ef2d055b9222269c3baa355
Nissan Azizov <na1733@nyu.edu>
2016-04-22 19:03:11 -0400

    Created FriendRequest model and FriendRequestLogic. Implemented methods Create, Accept and Decline FriendRequest in FriendRequestLogic

---
+++ ClassLibrary1/FriendRequestLogic.cs
@@ -0,0 +1,54 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public static class FriendRequestLogic
+    {
+        /// <summary>
+        /// This method creates a new friend request
+        /// </summary>
+        /// <param name="u"></param>
+        /// <param name="friend"></param>
+        /// <returns></returns>
+        public static FriendRequest CreateFriendRequest(User u, User friend)
+        {
+            var friendRequest = new FriendRequest();
+            friendRequest.User = u;
+            friendRequest.Friend = friend;
+
+            return friendRequest;
+        }
+
+        /// <summary>
+        /// This method allows a user to accept a friend request
+        /// </summary>
+        /// <param name="fr"></param>
+        public static void AcceptFriendRequest(FriendRequest fr)
+        {
+            User user1 = fr.User;
+            User user2 = fr.Friend;
+
+            Friends f = new Friends();
+            f.User1 = user1;
+            f.User2 = user2;
+
+            f.Friendships.Add(user1);
+            f.Friendships.Add(user1);         
+        }
+
+        /// <summary>
+        /// This method allows a user to decline a friend request
+        /// </summary>
+        /// <param name="fr"></param>
+        public static void DeclineFriendRequest(FriendRequest fr)
+        {
+            FriendRequest f = new FriendRequest();
+            f.FriendRequests.Remove(fr);
+        }
+    }
+}
---
+++ DataLayer/FriendRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class FriendRequest : DbContext
+    {
+        public virtual DbSet<FriendRequest> FriendRequests { get; set; }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public virtual User User { get; set; }
+        public virtual User Friend { get; set; }
+    }
+}
