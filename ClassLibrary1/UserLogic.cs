c2c585fb6707fd5e54f9c76559fc0fbd6de024cf
jw3877 <jw3877@nyu.edu>
2016-04-22 16:07:47 -0400

    Added PlayerLandOn() function to UserLogic

--- ClassLibrary1/UserLogic.cs
+++ ClassLibrary1/UserLogic.cs
@@ -39,5 +39,80 @@ namespace BusinessLogic
                     where bu.Board == board
                     select bu.Position).FirstOrDefault();
         }
+
+        /// <summary>
+        /// This function is called when a player lands on a property. It 
+        /// contains the property-based logic.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="b"></param>
+        /// <param name="pos"></param>
+        public static void PlayerLandedOn(this User user, Board b, int pos)
+        {
+
+            var boardUser = (from bu in user.BoardUsers
+                             where bu.BoardId == b.Id
+                             select bu).FirstOrDefault();
+
+            var prop = (from p in b.Properties
+                        where p.Position == pos
+                        select p).FirstOrDefault();
+
+
+            //
+            // The property is not owned.
+            //
+            if (prop.User == null)
+            {
+                // give option player to buy
+
+                bool buy = true;
+
+                if (buy)
+                {
+
+                    // if they can afford it
+                    if (boardUser.Money >= prop.Price)
+                    {
+                        boardUser.Money -= prop.Price;
+                        prop.User = user;
+
+                        // set prop.User ??
+                    }
+
+                    // else player can't afford property
+
+                }
+            }
+
+            //
+            // The property is owned.
+            //
+            else
+            {
+                // Someone else owns the property.
+                if (prop.User != user)
+                {
+                    int payment = prop.Rent;
+
+                    payment += prop.NumHotels * 10;
+                    payment += prop.NumHouses * 5;
+
+                    // Player has enough money to pay.
+                    if(boardUser.Money >= payment)
+                        boardUser.Money -= payment;
+
+                    // Player doesn't enough money to pay.
+                    //
+                    else
+                    {
+                        //TODO: Give player option to trade or mortgage properties
+                    }
+
+                }
+
+                // else player owns the property; do nothing
+            }
+        }
     }
 }
