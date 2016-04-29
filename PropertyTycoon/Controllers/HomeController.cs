0e1821c95e95cbde92ad446659d322bcd9d6bdaa
jw3877 <jw3877@nyu.edu>
2016-04-29 15:04:39 -0400

    - Added skeleton Play action/view - Added PlayViewModel - Added NotAuthorized View

--- PropertyTycoon/Controllers/HomeController.cs
+++ PropertyTycoon/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using BusinessLogic;
+using DataLayer;
+using PropertyTycoon.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,6 +12,8 @@ namespace PropertyTycoon.Controllers
 {
     public class HomeController : Controller
     {
+        private static GameContext gc = new GameContext();
+
         public ActionResult Index()
         {
             return View();
@@ -25,6 +31,74 @@ namespace PropertyTycoon.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Play()
+        {
+            if (!User.Identity.IsAuthenticated)
+                return View("NotAuthorized");
+
+
+            User user = (from u in gc.Users
+                         where u.UserName == User.Identity.Name
+                         select u).FirstOrDefault();
+
+            Board board = (from bu in gc.BoardUsers
+                           where bu.User == user
+                           select bu.Board).FirstOrDefault();
+
+            PlayViewModel pvm = new PlayViewModel()
+            {
+                MyTurn = board.GetPlayerWithCurrentTurn() == user
+            };
+
+            return View(pvm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Play(string button)
+        {
+            if (!User.Identity.IsAuthenticated)
+                return View("NotAuthorized");
+
+            User user = (from u in gc.Users
+                         where u.UserName == User.Identity.Name
+                         select u).FirstOrDefault();
+
+            Board board = (from bu in gc.BoardUsers
+                           where bu.User == user
+                           select bu.Board).FirstOrDefault();
+
+            switch (button)
+            {
+                case "Roll":
+                    {
+                        if (board.GetPlayerWithCurrentTurn() == user)
+                        {
+                            bool doubles;
+                            int roll;
+
+                            board.MakeCurrentPlayerMove(out doubles, out roll);
+                        }
+                        break;
+                    }
+                case "BuyProperty": break;
+                case "MortgageProperty":break;
+                case "BuyHouse": break;
+                case "BuyHotel": break;
+                case "SellHouse": break;
+                case "SellHotel": break;
+
+                default: return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            PlayViewModel pvm = new PlayViewModel()
+            {
+                MyTurn = false
+            };
+
+            return View(pvm);
         }
     }
 }
---
+++ PropertyTycoon/Models/PlayViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PropertyTycoon.Models
+{
+    public class PlayViewModel
+    {
+        public string Status { get; set; }
+
+        public bool MyTurn { get; set; }
+
+        public bool CanRoll { get; set; }
+
+        public bool CanBuyProperty { get; set; }
+
+        public bool CanMortgageProperty { get; set; }
+
+        public bool CanBuyHouse { get; set; }
+
+        public bool CanBuyHotel { get; set; }
+
+        public bool CanSellHouse { get; set; }
+
+        public bool CanSellHotel { get; set; }
+
+    }
+}
